4e228d8c46ee3677630da9492abb85eecfeca94b
Marcin Lisewski <marcin.lisewski@gmail.com>
2020-02-17 11:50:47 +0100

    Create wp-scripts-skeleton.js

---
+++ wp-scripts-skeleton.js
@@ -0,0 +1,32 @@
+;(function($){
+var scripts = {
+customVariable: 'custom Variable',
+
+
+ //custom function
+ customFunction: function(){
+ 
+ 
+ },
+ 
+  // all events goes here
+  events: function(){
+  
+  },
+  
+  // init events and other functions
+  init: function(){
+    this.events();
+  
+  }
+
+}
+
+
+    $( document ).ready(function() {
+        
+     scripts.init();
+
+    });
+    
+})(jQuery);
